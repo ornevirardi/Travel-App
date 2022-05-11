c3800c7a70034652d5743dfbd873fe0113161921
Orne Virardi <73016255+ornevirardi@users.noreply.github.com>
2022-05-11 16:26:49 -0700

    Update app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -11,33 +11,32 @@ async function getApiData(url = '', data = {}) {
     });
   
     try {
-      const newData = await response.JSON();
+      const newData = await response.json();
       console.log(newData);
       return newData;
     } catch (error) {
       console.log("error", error);
     }
   };  
-
   export {getApiData};
 
 //UpdateUI taken from my project: evaluate news NLP https://github.com/ornevirardi/NLP-testing/blob/main/src/client/js/formHandler.js
-  async function updateUI() {
-    const request = await fetch('http://localhost:1111/allData'); 
-    try {
-        const allData = await request.json();
-        console.log(allData);
-        document.getElementById("temp").innerHTML = `Current Temperature: ${allData.temp}`;
-        console.log(allData.temp);
-        document.getElementById("maxtemp").innerHTML = `Forecast Max Temperature: ${allData.max_temp}`;
-        console.log(allData.max_temp);
-        document.getElementById("mintemp").innerHTML = `Forecast Min Temperature: ${allData.min_temp}`;
-        console.log(allData.min_temp);
-        document.getElementById("precip").innerHTML = `Forecast Precipitations - Rain: ${allData.precip}`;
-        console.log(allData.precip);
-        document.getElementById('imagen').src = allData.foto;
-    } catch (error) {
-        console.log("UI data could not be updated", error);
-    }
+async function updateUI() {
+  const request = await fetch('http://localhost:1111/allData');
+  try {
+    const allData = await request.json();
+    console.log(allData);
+    document.getElementById("temp").innerHTML = `Current Temperature: ${allData.temp}`;
+    console.log(allData.temp);
+    document.getElementById("maxtemp").innerHTML = `Forecast Max Temperature: ${allData.maxTemp}`;
+    console.log(allData.max_temp);
+    document.getElementById("mintemp").innerHTML = `Forecast Min Temperature: ${allData.minTemp}`;
+    console.log(allData.min_temp);
+    document.getElementById("precip").innerHTML = `Forecast Precipitations - Rain: ${allData.precip}`;
+    console.log(allData.precip);
+    document.getElementById('imagen').src = allData.foto;
+  } catch (error) {
+    console.log("UI data could not be updated", error);
+  }
 }
 export { updateUI };
